40be81b0e1573e23130d41fdb3f2e348af86c652
Anna <anna.mcphee@gmail.com>
2021-07-25 10:16:08 +1200

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,10 +17,14 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
-    String language = args[0];
-    String country = args[1];
-    String name = args[2];
+
+    // create some ternary fallback values if num of args is incorrect
+    String language = (args.length > 0) ? args[0]: "en";
+    String country = (args.length > 1) ? args[1]: "NZ";
+    String name = (args.length > 2) ? args[2]: "Friend";
+
     Greeter greeter = new Greeter(language, country, name);
     System.out.println(greeter.sayHello());
+
   }
 }
